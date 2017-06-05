bdbc46eb3cd2ad53cda5acd2135190857ea926f4
Hyacinthe Hamon <hamon.hyacinthe@gmail.com>
2017-06-05 13:14:46 +1000

    first commit

--- index.ios.js
+++ index.ios.js
@@ -1,30 +1,38 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import Animation from 'lottie-react-native';
 
-import anim from './assets/soda_loader.json';
+import anim from './assets/touch_id.json';
 
 export default class lottieloader extends Component {
   componentDidMount() {
     this.animation.play();
   }
 
+  handleTouchID () {
+ }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Lottie Animations :-)</Text>
+        <Text style={styles.welcome}>Would you like to use Touch ID?</Text>
         <View>
           <Animation
             ref={animation => {
               this.animation = animation;
             }}
             style={{
-              width: 80,
-              height: 80
+              width: 200,
+              height: 160
             }}
-            loop={true}
+            loop={false}
             source={anim}
           />
+        </View>
+        <View style={styles.footer}>
+          <TouchableHighlight onPress={this.handleTouchID()}>
+            <Text style={styles.buttonText}>Use Touch ID</Text>
+          </TouchableHighlight>
         </View>
 
       </View>
@@ -37,13 +45,32 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#A6207E'
+    backgroundColor: '#030A16'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
     color: '#ffffff'
+  },
+  buttonText: {
+      color:"#fff",
+      fontSize: 20,
+  },
+  text: {
+      color: '#fff',
+      textAlign: 'center',
+  },
+  footer: {
+      position: 'absolute',
+      flex:1,
+      height: 50,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: '#21CE99',
+      justifyContent: 'center',
+      alignItems: 'center'
   }
 });
 
